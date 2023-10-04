65277622c203ecd078f2aa93ec374c3a4b77460f
StephaniaIbarraL <almaibarralozano@gmail.com>
2023-10-03 20:06:54 -0600

    Added todo components

---
+++ src/components/ListItem.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+function ListItem({text, onDelete}){
+
+    const [checked, setChecked] = useState(false);
+
+    const handleCheck = ()=> {
+        setChecked(!checked); //toma el valor que esta en el check box, y lo cambia, si es falso lo cambia a verdadero, y si esta en verdadero lo cambia a falso
+    }
+    return (
+        <li className={`p-2 bg-indigo-800 ${checked ? 'text-green-400 line-through' : null}`}>
+        {text}<input type="checkbox" checked={checked} onChange={handleCheck}></input></li>
+    
+    )
+}
+
+export default ListItem;
+
