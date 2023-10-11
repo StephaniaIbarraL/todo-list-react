ca93495d7aec634404774e77d2e2a53481470d93
StephaniaIbarraL <almaibarralozano@gmail.com>
2023-10-10 19:25:11 -0600

    Added new todo function

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 function ListItem({text, onDelete}){
+    //console.log(props);
 
     const [checked, setChecked] = useState(false);
 
@@ -8,7 +9,7 @@ function ListItem({text, onDelete}){
         setChecked(!checked); //toma el valor que esta en el check box, y lo cambia, si es falso lo cambia a verdadero, y si esta en verdadero lo cambia a falso
     }
     return (
-        <li className={`p-2 bg-indigo-800 ${checked ? 'text-green-400 line-through' : null}`}>
+        <li className={` rounded-md p-2 bg-indigo-800 ${checked ? 'text-green-400 line-through' : null}`}>
         {text}<input type="checkbox" checked={checked} onChange={handleCheck}></input></li>
     
     )
