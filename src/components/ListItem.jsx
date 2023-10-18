e4daf09610b34153d44f75efe29a2a01454075f8
StephaniaIbarraL <almaibarralozano@gmail.com>
2023-10-17 19:53:24 -0600

    Added fetch

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -10,9 +10,18 @@ function ListItem({text, onDelete}){
         //init component
         console.log("Init")
         return ()=>{
-            console.log("cleanup")
+            console.log("cleanup");
         }
     },[]);
+
+    useEffect(()=>{
+        //checar el estado del la casilla
+        console.log("checked", checked);
+        if(checked) alert ("Check");
+        
+    },[checked]);
+
+
 
     const handleCheck = ()=> {
         setChecked(!checked); //toma el valor que esta en el check box, y lo cambia, si es falso lo cambia a verdadero, y si esta en verdadero lo cambia a falso
--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -1,14 +1,14 @@
-import { useState,useRef, useEffect } from "react";
+import { useState, useRef, useEffect } from "react";
 import ListItem from "../components/ListItem";
 import { v4 as uuidv4 } from "uuid";
 
-function Todo (){
-    //Use effect - sirve para usar efectos en nuestra pagina 
-    // useState devuelve un array con dos cosas 
-    // useState->[state, setState]
-    const [todos, setTodos]= useState([]);
-    const inputRef=useRef(null);
-/*
+function Todo() {
+  //Use effect - sirve para usar efectos en nuestra pagina
+  // useState devuelve un array con dos cosas
+  // useState->[state, setState]
+  const [todos, setTodos] = useState([]);
+  const inputRef = useRef(null);
+  /*
     //DependencyList is null exec once
     //cuando la dependencia esta vacia se ejecuta solo una vez
     useEffect(()=> {
@@ -18,7 +18,7 @@ function Todo (){
     },[]);
 */
 
-/*
+  /*
     //without DependencyList exec each time state changes
     useEffect(()=> {
         //check size of container
@@ -26,56 +26,73 @@ function Todo (){
     },[]);
 */
 
-    useEffect(()=> {
-        //check size of container
-        console.log("useEffect", todos);
-    },[todos]);
+  useEffect(() => {
+    //check size of container
+    console.log("useEffect", todos);
+  }, [todos]);
 
-    //Add a new todo
-    const addTodo=() => {
-        const todoValue = inputRef.current.value;
-
-        const newTodo= {name: todoValue, id: uuidv4()}
-
-        console.log(todoValue);
-
-        console.log("before", todos);
-
-        setTodos([newTodo, ...todos]);
-
-        console.log("after", todos);
-
-        inputRef.current.value="";
+  useEffect(() => {
+    const getTodos = () => {
+      fetch("https://jsonplaceholder.typicode.com/todos/1")
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+        });
     };
+    getTodos();
+  }, []);
 
-    const deleteTodo=(id) =>{
-        // detete todo by id
-        //filter items different to given id
-        setTodos(todos.filter((item)=> item.id!== id ))
+  //Add a new todo
+  const addTodo = () => {
+    const todoValue = inputRef.current.value;
 
+    const newTodo = { name: todoValue, id: uuidv4() };
 
-    }
+    console.log(todoValue);
 
+    console.log("before", todos);
 
+    setTodos([newTodo, ...todos]);
 
-    return (
-        <div className="flex flex-col gap-2">
-            <div  className="flex gap-2">
-                <input ref={inputRef} type="text" className="bg-[444] rounded-md p-2 text-black" />
-                <button onClick={addTodo} className="rounded-md bg-indigo-600 px-4 py-2" >Add todo</button>
+    console.log("after", todos);
 
-            </div>
-            <ul className="flex flex-col gap-2" >
-                {
-                    todos.map((item)=> {
-                        return <ListItem key={item.id} text={item.name} onDelete={()=> deleteTodo(item.id)}/>
-                    })
-                }            
-                
-            </ul>
-        </div>
-    )
+    inputRef.current.value = "";
+  };
+
+  const deleteTodo = (id) => {
+    // detete todo by id
+    //filter items different to given id
+    setTodos(todos.filter((item) => item.id !== id));
+  };
+
+  return (
+    <div className="flex flex-col gap-2">
+      <div className="flex gap-2">
+        <input
+          ref={inputRef}
+          type="text"
+          className="rounded-md bg-[444] p-2 text-black"
+        />
+        <button
+          onClick={addTodo}
+          className="rounded-md bg-indigo-600 px-4 py-2"
+        >
+          Add todo
+        </button>
+      </div>
+      <ul className="flex flex-col gap-2">
+        {todos.map((item) => {
+          return (
+            <ListItem
+              key={item.id}
+              text={item.name}
+              onDelete={() => deleteTodo(item.id)}
+            />
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default Todo;
-
